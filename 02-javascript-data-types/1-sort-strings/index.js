4b002202422d708e22b4c4e1fadf1370ee72a81a
dontshoot11 <megadestroyer95@gmail.com>
2020-12-16 21:55:36 +0300

    added function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,17 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+    const newArr = arr.concat();
 
+    function smartSort(first, second) {
+        let fN = first.normalize(),
+            sN = second.normalize();
+        return fN.localeCompare(sN, "ru", { caseFirst: "upper" });
+    }
+
+    if (param === "desc") {
+        return newArr.sort(smartSort).reverse();
+    }
+    return newArr.sort(smartSort);
 }
