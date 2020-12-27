22bfebb2668be5474812a3403bb59f11ea287e0b
dontshoot11 <megadestroyer95@gmail.com>
2020-12-27 19:12:32 +0300

    notification add

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,52 @@
 export default class NotificationMessage {
+    constructor(text = "", { type = "", duration = Number } = {}) {
+        this.type = type;
+        this.duration = duration;
+        this.text = text;
+        this.render();
+    }
 
+    get template() {
+        return ` <div class="notification
+          ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+          ${this.text}
+          </div>
+        </div>
+      </div>
+        `;
+    }
+
+    render() {
+        const element = document.createElement("div");
+        element.innerHTML = this.template;
+        this.element = element.firstElementChild;
+    }
+
+    show(target) {
+        let notification = document.querySelector(".notification");
+        if (notification) {
+            notification.remove();
+            this.append(target);
+            setTimeout(() => this.remove(), this.duration);
+        } else {
+            this.append(target);
+            setTimeout(() => this.remove(), this.duration);
+        }
+    }
+
+    append(target) {
+        target ? target.append(this.element) : document.body.append(this.element);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.element = null;
+    }
 }
