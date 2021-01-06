06ac9bf468e48b08133dcb144a7bb85cc522b73d
dontshoot11 <megadestroyer95@gmail.com>
2021-01-06 20:51:14 +0300

    missing file fix

--- 06-events-practice/3-double-slider/index.spec.js
+++ 06-events-practice/3-double-slider/index.spec.js
@@ -83,14 +83,8 @@ describe('events-practice/double-slider', () => {
       bubbles: true
     });
 
-    const up = new MouseEvent('pointerup', {
-      clientX: 0,
-      bubbles: true
-    });
-
     leftSlider.dispatchEvent(down);
     leftSlider.dispatchEvent(move);
-    leftSlider.dispatchEvent(up);
 
     expect(leftBoundary).toHaveTextContent(doubleSlider.min);
   });
@@ -213,7 +207,7 @@ describe('events-practice/double-slider', () => {
     });
 
     const move = new MouseEvent('pointermove', {
-      clientX: 200,
+      clientX: 300,
       bubbles: true
     });
 
@@ -228,7 +222,7 @@ describe('events-practice/double-slider', () => {
     const customEvent = spyDispatchEvent.mock.calls[0][0];
 
     expect(spyDispatchEvent).toHaveBeenCalled();
-    expect(customEvent.detail).toEqual({ from: 120, to: 150 });
+    expect(customEvent.detail).toEqual({ from: 130, to: 150 });
   });
 
   it('should have ability to be destroyed', () => {
