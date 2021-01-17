413bbb2d828498660e52dbb6d432ce21a9fe7eb1
dontshoot11 <megadestroyer95@gmail.com>
2021-01-17 19:01:16 +0300

    fix

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -2,13 +2,50 @@ export default class SortableList {
     element = document.createElement("ul");
     constructor({ items } = {}) {
         this.items = items;
+        this.array = this.items.concat();
         this.render();
     }
 
-    render() {
-        console.log(this.items);
-        for (let i = 0; i < this.items.length; i++) {
-            this.element.append(this.items[i]);
+    render(array = this.items) {
+        for (let i = 0; i < array.length; i++) {
+            array[i].style.position = "relative";
+            array[i].style.top = "auto";
+            this.element.append(array[i]);
+            this.dragInit(array[i]);
         }
+    }
+
+    updateRender(item) {
+        console.log(this.array.indexOf(item));
+        // console.log(this.array.indexOf(droppableBelow));
+    }
+
+    dragInit(item) {
+        item.ondragstart = function() {
+            return false;
+        };
+        item.onmousedown = (event) => {
+            item.style.position = "absolute";
+            item.style.zIndex = 1000;
+            moveAt(event.pageY);
+
+            function moveAt(pageY) {
+                item.style.top = pageY - item.offsetHeight / 2 + "px";
+            }
+
+            function onMouseMove(event) {
+                moveAt(event.pageY);
+            }
+
+            document.addEventListener("mousemove", onMouseMove);
+
+            item.onmouseup = (event) => {
+                let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+                console.log(elemBelow);
+                this.updateRender(event.target.parentElement);
+                document.removeEventListener("mousemove", onMouseMove);
+                item.onmouseup = null;
+            };
+        };
     }
 }
