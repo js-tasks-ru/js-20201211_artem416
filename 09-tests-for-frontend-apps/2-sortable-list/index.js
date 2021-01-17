90e63c4afb79a5bb71796433d43ba97a1c2962fb
dontshoot11 <megadestroyer95@gmail.com>
2021-01-17 15:19:02 +0300

    тестовый коммит чтобы понять не сломалось ли что

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,14 @@
 export default class SortableList {
+    element = document.createElement("ul");
+    constructor({ items } = {}) {
+        this.items = items;
+        this.render();
+    }
 
+    render() {
+        console.log(this.items);
+        for (let i = 0; i < this.items.length; i++) {
+            this.element.append(this.items[i]);
+        }
+    }
 }
